e1b44b08a34259c7a4e111a4edac87b485779681
Stephane Miginiac <smiginiac@talentsoft.com>
2014-06-11 16:12:43 +0200

    Delete useless files

--- ObjectModel/FakeProfileSimulator.cs
+++ ObjectModel/FakeProfileSimulator.cs
@@ -21,34 +21,30 @@ namespace ObjectModel
             var c5 = new Competence("Orchard", 5);
             c5.Type = CompetenceType.TechnicalProject;
 
-            var r1 = new IntechRealisation("TiDoi", 1, "TiDoi.png");
+            var r1 = new Realisation("TiDoi", 1, "TiDoi.png");
             r1.Description = @"Tidoi est un projet innovant et ayant un objectif bien réel. C'est un site web qui permet
             de comparer les bons plans concernant les activités tels que la restauration où les bars.
             Réalisé sur deux semestres, cela a été une vraie opportunité pour nous de démontrer notre savoir-faire et nos compétences que ce soit sur le plan technique ou humain.
             Ayant été chef de projet lors du premier semestre, j'ai pu me rendre compte des difficultés de la gestion d'une équipe ainsi que d'une vraie relation client.";
             r1.Type = RealisationType.Technical;
-            r1.Semester = IntechSemester.S3;
 
-            var r2 = new IntechRealisation("BlogOn", 2, "blogon.png");
+            var r2 = new Realisation("BlogOn", 2, "blogon.png");
             r2.Description = @"Blog'On est un générateur de blogs. Il permet de créer et d'administrer son propre blog de manière autonome. Il est possible de 
             créer des utilisateurs/administrateurs, de leur donner des droits, créer des articles ,changer le style du blog, et bien d'autres choses.";
             r2.Type = RealisationType.Technical;
-            r2.Semester = IntechSemester.S2;
 
-            var r3 = new IntechRealisation("MKS", 3, "LogoMKS.png");
+            var r3 = new Realisation("MKS", 3, "LogoMKS.png");
             r3.Description = @"Notre mission pour le semestre a été d'ajouter un module à une solution existante, MKS. Ce dernier a pour but de faciliter la gestion
             d'IntechInfo. Notre module avait pour objectif de concevoir et développer un outil permettant, à l'équipe pédagogique et administrative du groupe 
             ESIEA, d'alimenter et de gérer les catalogues de cours d'IN'TECH INFO et de l'ESIEA.";
             r3.Type = RealisationType.Technical;
-            r3.Semester = IntechSemester.S5;
 
-            var r4 = new IntechRealisation("Web Academy", 4, "webacademy.png");
+            var r4 = new Realisation("Web Academy", 4, "webacademy.png");
             r4.Description = @"Pour ce projet de semestre 3, notre objectif a été de former les étudiants de 1er semestre d'INTECH INFO au HTML et au CSS.
             Le défi était de taille puisque leur apprentissage de ces technologies dépendait uniquement de nous. Nous leur avons donc dispensé quatre cours de 2h,
             alliant parties théoriques (à l'aide de présentations PowerPoint) et pratiques (TP sur leur poste). Pour les TPs, l'objectif était de réaliser une galerie photo qui évoluait au fil des cours. A l'aide de ce
             projet 'fil rouge', les étudiants se rendaient mieux compte de leur propre évolution.";
             r4.Type = RealisationType.Human;
-            r4.Semester = IntechSemester.S3;
 
             c1.AddRealisations(new List<Realisation>{ r1, r3 });
             c2.AddRealisations(new List<Realisation> { r1, r2, r3 });
