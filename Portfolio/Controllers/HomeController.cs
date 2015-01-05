cde96635206d8396c97c076bdbe6344d9d03ce58
Stephane Miginiac <stephonline13@gmail.com>
2015-01-05 15:26:30 +0100

    Contact page works now

--- Portfolio/Controllers/HomeController.cs
+++ Portfolio/Controllers/HomeController.cs
@@ -11,6 +11,10 @@ using System.Web.Mvc;
 
 namespace Portfolio.Controllers
 {
+    using System.Net.Mail;
+
+    using Portfolio.ViewModels;
+
     public class HomeController : BaseController
     {
         public HomeController()
@@ -25,7 +29,27 @@ namespace Portfolio.Controllers
 
         public ActionResult Contact()
         {
-            return View("Contact");
+            return View("Contact", new ContactViewModel());
+        }
+
+        [HttpPost]
+        public ActionResult Contact(ContactViewModel model)
+        {
+            MailMessage mail = new MailMessage();
+            SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+
+            mail.From = new MailAddress(model.YourMail);
+            mail.To.Add("stephane.miginiac@gmail.com");
+            mail.Subject = "Message from : " + model.YourMail;
+            mail.Body = model.YourMessage;
+
+            SmtpServer.Port = 587;
+            SmtpServer.Credentials = new System.Net.NetworkCredential("stephmigi.portfolio@gmail.com", "halflife#A");
+            SmtpServer.EnableSsl = true;
+
+            SmtpServer.Send(mail);
+
+            return this.View();
         }
     }
 }
---
+++ Portfolio/ViewModels/ContactViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Portfolio.ViewModels
+{
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+
+    public class ContactViewModel
+    {
+        [Required]
+        [RegularExpression(@"^([0-9a-zA-Z]([\+\-_\.][0-9a-zA-Z]+)*)+@(([0-9a-zA-Z][-\w]*[0-9a-zA-Z]*\.)+[a-zA-Z0-9]{2,3})$",
+        ErrorMessage = "Le format de l'adresse email est invalide.")]
+        [DisplayName("Votre adresse mail :")]
+        public string YourMail { get; set; }
+
+        [Required]
+        [DisplayName("Votre message :")]
+        public string YourMessage { get; set; }
+    }
+}
